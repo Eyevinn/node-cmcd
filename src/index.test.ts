9f714eecd2bdb54b5accb8f7af4b79593e2df669
Jonas Birmé <jonas.birme@eyevinn.se>
2022-10-13 23:09:00 -0700

    feat: can create a payload from search param

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,28 +1,28 @@
-import { createPayload, CMCDString, CMCDBoolean, CMCDInteger, CMCDDecimal, CMCDEncodedBitrate, CMCDBufferLength, CMCDBufferStarvation, CMCDContentId, CMCDObjectDuration, CMCDDeadline, CMCDMeasuredThroughput, CMCDNextObjectRequest, CMCDObjectType, CMCDNextRangeRequest, CMCDObjectTypeToken, CMCDHdr, CMCDPlaybackRate, CMCDRequestedMaximumThroughput, CMCDStreamingFormat, CMCDStreamingFormatToken, CMCDSessionId, CMCDStreamType, CMCDStreamTypeToken, CMCDStartup, CMCDTopBitrate, CMCDVersion } from ".";
+import { createPayload, CMCDString, CMCDBoolean, CMCDInteger, CMCDDecimal, CMCDEncodedBitrate, CMCDBufferLength, CMCDBufferStarvation, CMCDContentId, CMCDObjectDuration, CMCDDeadline, CMCDMeasuredThroughput, CMCDNextObjectRequest, CMCDObjectType, CMCDNextRangeRequest, CMCDObjectTypeToken, CMCDHdr, CMCDPlaybackRate, CMCDRequestedMaximumThroughput, CMCDStreamingFormat, CMCDStreamingFormatToken, CMCDSessionId, CMCDStreamType, CMCDStreamTypeToken, CMCDStartup, CMCDTopBitrate, CMCDVersion, CMCDKey } from ".";
 
 describe("CMCD Basic Type", () => {
   test("string", () => {
-    const cmcdString = new CMCDString("cid", CMCDHdr.session, "foobar");
+    const cmcdString = new CMCDString(CMCDKey.cid, CMCDHdr.session, "foobar");
     expect(cmcdString.value).toEqual("foobar");
     expect(cmcdString.toString()).toEqual('cid="foobar"');
   });
 
   test("boolean", () => {
-    const cmcdBooleanTrue = new CMCDBoolean("bs", CMCDHdr.status, true);
+    const cmcdBooleanTrue = new CMCDBoolean(CMCDKey.bs, CMCDHdr.status, true);
     expect(cmcdBooleanTrue.value).toEqual(true);
     expect(cmcdBooleanTrue.toString()).toEqual("bs");
 
-    const cmcdBooleanFalse = new CMCDBoolean("bs", CMCDHdr.status, false);
+    const cmcdBooleanFalse = new CMCDBoolean(CMCDKey.bs, CMCDHdr.status, false);
     expect(cmcdBooleanFalse.value).toEqual(false);
     expect(cmcdBooleanFalse.toString()).toEqual("");
   });
 
   test("number", () => {
-    const cmcdInteger = new CMCDInteger("br", CMCDHdr.object, 2000);
+    const cmcdInteger = new CMCDInteger(CMCDKey.br, CMCDHdr.object, 2000);
     expect(cmcdInteger.value).toEqual(2000);
     expect(cmcdInteger.toString()).toEqual("br=2000");
 
-    const cmcdDecimal = new CMCDDecimal("d", CMCDHdr.object, 4.5);
+    const cmcdDecimal = new CMCDDecimal(CMCDKey.d, CMCDHdr.object, 4.5);
     expect(cmcdDecimal.value).toEqual(4.5);
     expect(cmcdDecimal.toString()).toEqual("d=4.5")
   });
@@ -88,12 +88,17 @@ describe("CMCD Type", () => {
 
 });
 
-/*
 describe("CMCD Payload", () => {
   test("can be constructed from URLSearchParams instance", () => {
-    const params = new URLSearchParams("CMCD=br%3D3200%2Cbs%2Cd%3D4004%2Cmtp%3D25400%2Cot%3Dv%2Crtp%3D15000%2Csid%3D%226e2fb550-c457-11e9-bb97-0800200c9a66%22%2Ctb%3D6000");
-    const payload = createPayload(params);
+    const url: URL = new URL("https://my.domain/f?CMCD=br%3D3200%2Cbs%2Cd%3D4004%2Cmtp%3D25400%2Cot%3Dv%2Crtp%3D15000%2Csid%3D%226e2fb550-c457-11e9-bb97-0800200c9a66%22%2Ctb%3D6000");
+    const payload = createPayload(url.searchParams);
     expect(payload.sessionId).toEqual('6e2fb550-c457-11e9-bb97-0800200c9a66');
+    expect(payload.bufferStarvation).toEqual(true);
+    expect(payload.encodedBitrate).toEqual(3200);
+    expect(payload.objectDuration).toEqual(4004);
+    expect(payload.measuredThroughput).toEqual(25400);
+    expect(payload.objectType).toEqual(CMCDObjectTypeToken.video);
+    expect(payload.requestedMaximumThroughput).toEqual(15000);
+    expect(payload.topBitrate).toEqual(6000);
   });
 });
-*/
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,44 @@
+export enum CMCDKey {
+  br = "br",
+  bl = "bl",
+  bs = "bs",
+  cid = "cid",
+  d = "d",
+  dl = "dl",
+  mtp = "mtp",
+  nor = "nor",
+  nrr = "nrr",
+  ot = "ot",
+  pr = "pr",
+  rtp = "rtp",
+  sf = "sf",
+  sid = "sid",
+  st = "st",
+  su = "su",
+  tb = "tb",
+  v = "v"
+}
+const CMCD_MAP = {
+  "br": "encodedBitrate",
+  "bl": "bufferLength",
+  "bs": "bufferStarvation",
+  "cid": "contentId",
+  "d": "objectDuration",
+  "dl": "deadline",
+  "mtp": "measuredThroughput",
+  "nor": "nextObjectRequest",
+  "nrr": "nextRangeRequest",
+  "ot": "objectType",
+  "pr": "playbackRate",
+  "rtp": "requestedMaximumThroughput",
+  "sf": "streamingFormat",
+  "sid": "sessionId",
+  "st": "streamType",
+  "su": "startup",
+  "tb": "topBitrate",
+  "v": "version",
+};
+
 export enum CMCDObjectTypeToken {
   manifest = "m",
   audio = "a",
@@ -27,17 +68,17 @@ export enum CMCDHdr {
 }
 
 class CMCDValue<T> {
-  protected _key: string;
+  protected _key: CMCDKey;
   protected _header: CMCDHdr;
   protected _value: T;
 
-  constructor(key: string, header: CMCDHdr, value: T) {
+  constructor(key: CMCDKey, header: CMCDHdr, value: T) {
     this._key = key;
     this._header = header;
     this._value = value; 
   }
 
-  get key(): string { return this._key }
+  get key(): string { return this._key.toString() }
   get header(): string { return this._header }
   get value(): T { return this._value }
 
@@ -56,28 +97,28 @@ export class CMCDDecimal extends CMCDValue<Number> { }
 
 // CMCD Types
 export class CMCDEncodedBitrate extends CMCDInteger {
-  constructor(value: Number) { super("br", CMCDHdr.object, value); }
+  constructor(value: Number) { super(CMCDKey.br, CMCDHdr.object, value); }
 }
 export class CMCDBufferLength extends CMCDInteger {
-  constructor(value: Number) { super("bl", CMCDHdr.request, value); }
+  constructor(value: Number) { super(CMCDKey.bl, CMCDHdr.request, value); }
 }
 export class CMCDBufferStarvation extends CMCDBoolean {
-  constructor(value: Boolean) { super("bs", CMCDHdr.status, value); }
+  constructor(value: Boolean) { super(CMCDKey.bs, CMCDHdr.status, value); }
 }
 export class CMCDContentId extends CMCDString {
-  constructor(value: string) { super("cid", CMCDHdr.session, value); }
+  constructor(value: string) { super(CMCDKey.cid, CMCDHdr.session, value); }
 }
 export class CMCDObjectDuration extends CMCDInteger {
-  constructor(value: Number) { super("d", CMCDHdr.object, value); }
+  constructor(value: Number) { super(CMCDKey.d, CMCDHdr.object, value); }
 }
 export class CMCDDeadline extends CMCDInteger {
-  constructor(value: Number) { super("dl", CMCDHdr.request, value); }
+  constructor(value: Number) { super(CMCDKey.dl, CMCDHdr.request, value); }
 }
 export class CMCDMeasuredThroughput extends CMCDInteger {
-  constructor(value: Number) { super("mtp", CMCDHdr.request, value); }
+  constructor(value: Number) { super(CMCDKey.mtp, CMCDHdr.request, value); }
 }
 export class CMCDNextObjectRequest extends CMCDString {
-  constructor(value: string) { super("nor", CMCDHdr.request, value); }
+  constructor(value: string) { super(CMCDKey.nor, CMCDHdr.request, value); }
   toString() {
     const q = {};
     q[this.key] = this.value;
@@ -87,36 +128,144 @@ export class CMCDNextObjectRequest extends CMCDString {
   }
 }
 export class CMCDNextRangeRequest extends CMCDString {
-  constructor(value: string) { super("nrr", CMCDHdr.request, value); }
+  constructor(value: string) { super(CMCDKey.nrr, CMCDHdr.request, value); }
 }
 export class CMCDObjectType extends CMCDValue<CMCDObjectTypeToken> {
-  constructor(value: CMCDObjectTypeToken) { super("ot", CMCDHdr.object, value) }
+  constructor(value: CMCDObjectTypeToken) { super(CMCDKey.ot, CMCDHdr.object, value) }
 }
 export class CMCDPlaybackRate extends CMCDDecimal {
-  constructor(value: Number) { super("pr", CMCDHdr.session, value); }
+  constructor(value: Number) { super(CMCDKey.pr, CMCDHdr.session, value); }
 }
 export class CMCDRequestedMaximumThroughput extends CMCDInteger {
-  constructor(value: Number) { super("rtp", CMCDHdr.status, value); }
+  constructor(value: Number) { super(CMCDKey.rtp, CMCDHdr.status, value); }
 }
 export class CMCDStreamingFormat extends CMCDValue<CMCDStreamingFormatToken> {
-  constructor(value: CMCDStreamingFormatToken) { super("sf", CMCDHdr.session, value) }
+  constructor(value: CMCDStreamingFormatToken) { super(CMCDKey.sf, CMCDHdr.session, value) }
 }
 export class CMCDSessionId extends CMCDString {
-  constructor(value: string) { super("sid", CMCDHdr.session, value); }
+  constructor(value: string) { super(CMCDKey.sid, CMCDHdr.session, value); }
 }
 export class CMCDStreamType extends CMCDValue<CMCDStreamTypeToken> {
-  constructor(value: CMCDStreamTypeToken) { super("st", CMCDHdr.session, value) }
+  constructor(value: CMCDStreamTypeToken) { super(CMCDKey.st, CMCDHdr.session, value) }
 }
 export class CMCDStartup extends CMCDBoolean {
-  constructor(value: Boolean) { super("su", CMCDHdr.request, value); }
+  constructor(value: Boolean) { super(CMCDKey.su, CMCDHdr.request, value); }
 }
 export class CMCDTopBitrate extends CMCDInteger {
-  constructor(value: Number) { super("tb", CMCDHdr.object, value); }
+  constructor(value: Number) { super(CMCDKey.tb, CMCDHdr.object, value); }
 }
 export class CMCDVersion extends CMCDInteger {
-  constructor(value: Number) { super("v", CMCDHdr.session, value); }
+  constructor(value: Number) { super(CMCDKey.v, CMCDHdr.session, value); }
 }
 
-export function createPayload() {
+function create<T>(ctor: { new (raw): T }, v): T {
+  return v !== undefined ? new ctor(v) : undefined;
+}
 
+export function createPayload(searchParams: URLSearchParams, override?) : Payload {
+  let values = [];
+  const cmcdString = searchParams.get("CMCD");
+  if (cmcdString) {
+    values = cmcdString.split(",");
+  }
+  let init = {};
+    
+  if (override) {
+    init = override;
+  }
+  values.forEach(kv => {
+    const [k, v] = kv.split("=");
+    if (init[CMCD_MAP[k]]) {
+      // do not overwrite overrides
+      return;
+    }
+    if (v) {
+      if (v[0] === '"' && v[v.length - 1] === '"') {
+        init[CMCD_MAP[k]] = v.substring(1, v.length - 1);
+      } else if (v.match(/^[0-9\.]*$/)) {
+        init[CMCD_MAP[k]] = Number(v);
+      } else {
+        init[CMCD_MAP[k]] = v;
+      }
+    } else {
+      init[CMCD_MAP[k]] = true;
+    }
+  });
+  return new Payload(init);
+}
+
+export class Payload {
+  private _encodedBitrate?: CMCDEncodedBitrate;
+  private _bufferLength?: CMCDBufferLength;
+  private _bufferStarvation?: CMCDBufferStarvation;
+  private _contentId?: CMCDContentId;
+  private _objectDuration?: CMCDObjectDuration;
+  private _deadline?: CMCDDeadline;
+  private _measuredThroughput?: CMCDMeasuredThroughput;
+  private _nextObjectRequest?: CMCDNextObjectRequest;
+  private _nextRangeRequest?: CMCDNextRangeRequest;
+  private _objectType?: CMCDObjectType;
+  private _playbackRate?: CMCDPlaybackRate;
+  private _requestedMaximumThroughput?: CMCDRequestedMaximumThroughput;
+  private _streamingFormat?: CMCDStreamingFormat;
+  private _sessionId?: CMCDSessionId;
+  private _streamType?: CMCDStreamType;
+  private _startup?: CMCDStartup;
+  private _topBitrate?: CMCDTopBitrate;
+  private _version?: CMCDVersion;
+
+  constructor(v) {
+    this._encodedBitrate = create(CMCDEncodedBitrate, v.encodedBitrate);
+    this._bufferLength = create(CMCDBufferLength, v.bufferLength);
+    this._bufferStarvation = create(CMCDBufferStarvation, v.bufferStarvation);
+    this._contentId = create(CMCDContentId, v.contentId);
+    this._objectDuration = create(CMCDObjectDuration, v.objectDuration);
+    this._deadline = create(CMCDDeadline, v.deadline);
+    this._measuredThroughput = create(CMCDMeasuredThroughput, v.measuredThroughput);
+    this._nextObjectRequest = create(CMCDNextObjectRequest, v.nextObjectRequest);
+    this._nextRangeRequest = create(CMCDNextRangeRequest, v.nextRangeRequest);
+    this._objectType = create(CMCDObjectType, v.objectType);
+    this._playbackRate = create(CMCDPlaybackRate, v.playbackRate);
+    this._requestedMaximumThroughput = create(CMCDRequestedMaximumThroughput, v.requestedMaximumThroughput);
+    this._streamingFormat = create(CMCDStreamingFormat, v.streamingFormat);
+    this._sessionId = create(CMCDSessionId, v.sessionId);
+    this._streamType = create(CMCDStreamType, v.streamType);
+    this._startup = create(CMCDStartup, v.startup);
+    this._topBitrate = create(CMCDTopBitrate, v.topBitrate);
+    this._version = create(CMCDVersion, v.version);
+  }
+
+  get encodedBitrate(): Number { return this._encodedBitrate.value }
+  get bufferLength(): Number { return this._bufferLength.value }
+  get bufferStarvation(): Boolean { return this._bufferStarvation.value }
+  get contentId(): string { return this._contentId.value }
+  get objectDuration(): Number { return this._objectDuration.value }
+  get deadline(): Number { return this._deadline.value }
+  get measuredThroughput(): Number { return this._measuredThroughput.value }
+  get nextObjectRequest(): string { return this._nextObjectRequest.value }
+  get nextRangeRequest(): string { return this._nextRangeRequest.value }
+  get objectType(): CMCDObjectTypeToken { return this._objectType.value }
+  get playbackRate(): Number { return this._playbackRate.value }
+  get requestedMaximumThroughput(): Number { return this._requestedMaximumThroughput.value }
+  get streamingFormat(): CMCDStreamingFormatToken { return this._streamingFormat.value }
+  get sessionId(): string { return this._sessionId.value }
+  get streamType(): CMCDStreamTypeToken { return this._streamType.value }
+  get startup(): Boolean { return this._startup.value }
+  get topBitrate(): Number { return this._topBitrate.value }
+  get version(): Number { return this._version.value }
+
+  toString() {
+    let kv = [];
+    Object.keys(CMCD_MAP).forEach(k => {
+      const key = '_' + CMCD_MAP[k];
+      if (this[key]) {
+        kv.push(this[key].toString());
+      }
+    });
+    if (kv.length > 0) {
+      return new URLSearchParams({ CMCD: kv.join(",") }).toString();
+    } else {
+      return "";
+    }
+  }
 }
